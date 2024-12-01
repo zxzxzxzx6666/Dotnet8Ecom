9e92b05ace7327ad7961e1f45273bf261201ff3f
Kai <zxzxzxzx6666@gmail.com>
2024-12-01 12:15:12 +0800

    [Log] Modify LogAdapter Save Log to local file

--- src/Infrastructure/Logging/LoggerAdapter.cs
+++ src/Infrastructure/Logging/LoggerAdapter.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+﻿using System.IO;
+using System;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.eShopWeb.Infrastructure.Logging;
@@ -6,6 +8,7 @@ namespace Microsoft.eShopWeb.Infrastructure.Logging;
 public class LoggerAdapter<T> : IAppLogger<T>
 {
     private readonly ILogger<T> _logger;
+    private static readonly string logFilePath = $"Logs/{DateTime.Now:yyyyMMdd}.log";
     public LoggerAdapter(ILoggerFactory loggerFactory)
     {
         _logger = loggerFactory.CreateLogger<T>();
@@ -14,10 +17,18 @@ public class LoggerAdapter<T> : IAppLogger<T>
     public void LogWarning(string message, params object[] args)
     {
         _logger.LogWarning(message, args);
+        WriteToFile("WARNING", message, args);
     }
 
     public void LogInformation(string message, params object[] args)
     {
         _logger.LogInformation(message, args);
+        WriteToFile("INFO", message, args);
+    }
+    private void WriteToFile(string level, string message, params object[] args)
+    {
+        var logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {string.Format(message, args)}";
+        Directory.CreateDirectory(Path.GetDirectoryName(logFilePath)); // 確保目錄存在
+        File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
     }
 }
